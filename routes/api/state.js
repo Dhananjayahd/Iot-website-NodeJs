bd871cb42661b31b90c1f2bcd63a07c33128e618
Dhananjayahd <dhananjayahd296@gmail.com>
2019-08-07 21:05:48 +0530

    test uptodate

--- routes/api/state.js
+++ routes/api/state.js
@@ -6,47 +6,41 @@ const path=require('path')
 
 const data = require('../../models/data')
 
-router.get('/data',(req,res)=>{
-    if(req.query.state=='ledon'){
-        const newdata1 = new data({
-            state:'ledon'
-        })
-        newdata1.save()
-        .then(console.log('done'))
-        .catch(err=>console.log(err))
-        data.find({state:'ledoff'})
-        .then((isfind)=>{
-            if(isfind){
-                data.deleteMany({state:'ledoff'}).then(()=>{
-                    console.log('deleted')
-                }).catch(err=>console.log(err))
-            }
-        })
-        .catch(err=>console.log(err))
-        res.render('message',{
-            message:'led is turned on'
-        })
-    }
-    else{
-        const newdata2 = new data({
-            state:'ledoff'
-        })
-        newdata2.save()
-        .then(console.log('done'))
-        .catch(err=>console.log(err))
-        data.find({state:'ledon'})
-        .then((isfind)=>{
-            if(isfind){
-                data.deleteMany({state:'ledon'}).then(()=>{
-                    console.log('deleted')
-                }).catch(err=>console.log(err))
-            }
-        })
-        .catch(err=>console.log(err))
-        res.render('message',{
-            message:'led is turned off'
-        })
-    }
+router.post('/ledon',(req,res)=>{
+   const newdata1 = new data({
+       state:'ledon'
+   })
+   newdata1.save()
+   .then(console.log('done'))
+   .catch(err=>console.log(err))
+   data.find({state:'ledoff'})
+   .then((isfind)=>{
+       if(isfind){
+           data.deleteMany({state:'ledoff'}).then(()=>{
+               console.log('deleted')
+           }).catch(err=>console.log(err))
+       }
+   })
+   .catch(err=>console.log(err))
+})
+
+router.post('/ledoff',(req,res)=>{
+    const newdata2 = new data({
+        state:'ledoff'
+    })
+    newdata2.save()
+    .then(console.log('done'))
+    .catch(err=>console.log(err))
+    data.find({state:'ledon'})
+    .then((isfind)=>{
+        if(isfind){
+            data.deleteMany({state:'ledon'}).then(()=>{
+                console.log('deleted')
+            }).catch(err=>console.log(err))
+        }
+    })
+    .catch(err=>console.log(err))
     
 })
+
 module.exports=router
